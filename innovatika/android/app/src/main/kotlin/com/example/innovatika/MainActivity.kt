cd61c748d8cb13b67aa1e265c8a4d2cfe7d0dc8a
Bishal <69814108+CodeWithBishal@users.noreply.github.com>
2025-11-02 01:04:58 +0530

    update

--- innovatika/android/app/src/main/kotlin/com/example/innovatika/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.innovatika
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
---
+++ innovatika/android/app/src/main/kotlin/com/innovatika/innovatika/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.innovatika.innovatika
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
